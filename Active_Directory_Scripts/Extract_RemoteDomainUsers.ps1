74671457e4348464bf5a5f772ab2c85f7efbf096
Danilo Vitelli Camillo <vitelli.camillo@gmail.com>
2021-02-22 00:05:41 -0300

    Minor changes in script

--- Active_Directory_Scripts/Extract_RemoteDomainUsers.ps1
+++ Active_Directory_Scripts/Extract_RemoteDomainUsers.ps1
@@ -3,6 +3,6 @@ $s = New-PSSession -HostName 3.82.44.56 -UserName administrator
 Invoke-Command -ScriptBlock {
     Import-module -PSSession $s -Name ActiveDirectory
     $result = @()
-    $result = Get-aduser -filter * -properties * | select-object SamAccountName
+    $result = Get-aduser -filter * -properties * | select-object SamAccountName, distinguishedName
     $result | Export-csv ~/Desktop/test.csv -NoTypeInformation -Encoding utf8
 }
